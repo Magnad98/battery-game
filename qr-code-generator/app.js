6fe563a1a77a94e80d10204ca53b0992f6c9e42b
Alexandru-Cristian Pop <alexandru.pop98@yahoo.com>
2022-05-29 15:30:33 +0300

    qr-code-generator finished

---
+++ qr-code-generator/app.js
@@ -0,0 +1,12 @@
+const http = require('http')
+const fs = require('fs')
+const port = 3000
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'content-type': 'text/html' })
+    fs.createReadStream('index.html').pipe(res)
+})
+
+server.listen(port, () => {
+    console.log(`Server is listening on port ${port}`)
+})
