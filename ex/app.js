5b2b792dbfb28802ede61054a4297f36cf849355
Alexandru-Cristian Pop <alexandru.pop98@yahoo.com>
2022-06-03 10:26:36 +0300

    updated express pusher server + qr code reader

--- ex/app.js
+++ ex/app.js
@@ -1,13 +1,23 @@
-const express = require('express')
+/*------------------------------EXPRESS-JS------------------------------*/
+const express = require("express");
 const app = express();
-const PORT = 3002
 
 app.use(express.json())
 
+app.get("/", (request, response) => {
+    response.sendFile(__dirname + "/index.html");
+});
+
+app.use(express.static(__dirname + "/public"));
+
+/*------------------------------HTTP-SERVER------------------------------*/
+const PORT = 3001
+
 app.listen(PORT, () => {
-    console.log(`it's alive on http://localhost:${PORT}`)
+    console.log(`[QR CODE READER STARTED AT PORT ${PORT}]`);
 })
 
+/*------------------------------PUSHER------------------------------*/
 app.post('/pusher', (req, res) => {
     const Pusher = require("pusher")
 
@@ -25,36 +35,4 @@ app.post('/pusher', (req, res) => {
         res.status(200).send("sent pusher request succesfully")
     })
 })
-
-// app.get('/pushergen', (req, res) => {
-//     const Pusher = require("pusher")
-
-//     const pusher = new Pusher({
-//         appId: "1414405",
-//         key: "1f05233fe6658e7cb61e",
-//         secret: "26e019fe152473429ae2",
-//         cluster: "eu",
-//         useTLS: true
-//     });
-
-//     data = {
-//         message: "bruh"
-//     }
-
-//     pusher.trigger("my-channel", "my-event", data).then(() => {
-//         res.status(200).send("sent pusher request succesfully")
-//     })
-// })
-
-// app.post('/tshirt/:id', (req, res) => {
-//     const { id } = req.params;
-//     const { logo } = req.body;
-
-//     if(!logo) {
-//         res.status(418).send({message: 'We need a logo!'})
-//     }
-
-//     res.send({
-//         tshirt: `tshirt with your ${logo} and ID ${id}`,
-//     })
-// })
+/*------------------------------------------------------------*/
